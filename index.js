78c73e4c8a9bf70ad82f9e484c7867b1d5353e0b
David Gilbert <dagilbert333@gmail.com>
2018-08-26 14:12:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function prependKitten(name){
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function removeLastKitten(){
-  kittens.slice
+  kittens.slice()
 }
 
 var kittens = ["Milo", "Otis", "Garfield"];
