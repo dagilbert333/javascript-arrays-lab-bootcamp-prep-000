0e81918890776e6dc2b39cd5e3715942cb25c885
David Gilbert <dagilbert333@gmail.com>
2018-08-26 13:59:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function appendKitten(name){
+  
+}
